4ff5b32c5d6abd43becbe69690a6c977e61cb526
Sadid Alam <sadidalam14@gmail.com>
2026-01-08 22:34:33 +0530

    changes portfolio path

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Portfolio from "../src/pages/PortFolio.jsx";
+import Portfolio from "./pages/PortFolio";
 
 const App = () => {
   return (
